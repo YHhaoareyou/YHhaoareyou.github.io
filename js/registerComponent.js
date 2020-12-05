90aeba7bf8be7dfb194bafe7087c63913a88731e
Hu Yong Hao <mlapenang1998@gmail.com>
2020-12-06 00:16:13 +0900

    test

--- js/registerComponent.js
+++ js/registerComponent.js
@@ -1,27 +1,27 @@
 AFRAME.registerComponent("markerhandler", {
   init: function() {
     this.el.sceneEl.addEventListener("markerFound", e => {
-      const locationName = e.target.getAttribute("location");
-
-      if (document.getElementById("newPaintCanvas").style.display !== "block") {
-        // Display painting switching buttons
-        var prevImgButton = document.getElementById("prevImgButton");
-        var nextImgButton = document.getElementById("nextImgButton");
-        prevImgButton.style.display = "block";
-        nextImgButton.style.display = "block";
-        prevImgButton.setAttribute("name", locationName);
-        nextImgButton.setAttribute("name", locationName);
-
-        var openCanvasButton = document.getElementById("openCanvasButton");
-        openCanvasButton.style.display = "block";
-        openCanvasButton.addEventListener("click", e => {
-          openCanvasButton.style.display = "none";
-          document.getElementById("newPaintCanvas").style.display = "block";
-          document.getElementById("closeCanvasButton").style.display = "block";
-          document.getElementById("colorPanel").style.display = "block";
-          document.getElementById("save").setAttribute("name", locationName);
-        });
-      }
+      // const locationName = e.target.getAttribute("location");
+      //
+      // if (document.getElementById("newPaintCanvas").style.display !== "block") {
+      //   // Display painting switching buttons
+      //   var prevImgButton = document.getElementById("prevImgButton");
+      //   var nextImgButton = document.getElementById("nextImgButton");
+      //   prevImgButton.style.display = "block";
+      //   nextImgButton.style.display = "block";
+      //   prevImgButton.setAttribute("name", locationName);
+      //   nextImgButton.setAttribute("name", locationName);
+      //
+      //   var openCanvasButton = document.getElementById("openCanvasButton");
+      //   openCanvasButton.style.display = "block";
+      //   openCanvasButton.addEventListener("click", e => {
+      //     openCanvasButton.style.display = "none";
+      //     document.getElementById("newPaintCanvas").style.display = "block";
+      //     document.getElementById("closeCanvasButton").style.display = "block";
+      //     document.getElementById("colorPanel").style.display = "block";
+      //     document.getElementById("save").setAttribute("name", locationName);
+      //   });
+      // }
     });
     this.el.sceneEl.addEventListener("markerLost", e => {
       document.getElementById("openCanvasButton").style.display = "none";
