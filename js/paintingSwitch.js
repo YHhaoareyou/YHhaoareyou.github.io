6244e7a1351b238f3be9181b9ac1324dd160d0b3
Hu Yong Hao <mlapenang1998@gmail.com>
2020-12-06 00:01:32 +0900

    test painting switching button

--- js/paintingSwitch.js
+++ js/paintingSwitch.js
@@ -1,57 +1,35 @@
 function switchToPrevPainting(e) {
   const locationName = e.target.getAttribute("name");
-  console.log(locationName);
   var nft = document.getElementById("nft_" + locationName);
-  console.log(nft);
   const imgCnt = nft.getAttribute("imgCnt");
-  console.log(imgCnt);
 
   // hide current image
   var currentImg = nft.querySelector("a-image[name='currentImg']");
-  console.log(currentImg);
-  console.log(currentImg.getAttribute("visible"));
   currentImg.setAttribute("visible", "false");
   currentImg.setAttribute("name", "");
-  console.log(currentImg);
-  console.log(currentImg.getAttribute("visible"));
 
   // display prev image
   const currentImgIndex = parseInt(currentImg.getAttribute("index"));
   const prevImgIndex = currentImgIndex === 0 ? imgCnt - 1 : currentImgIndex - 1;
   var prevImg = nft.querySelector("a-image[index='" + prevImgIndex + "']");
-  console.log(prevImg);
-  console.log(prevImg.getAttribute("visible"));
   prevImg.setAttribute("visible", "true");
   prevImg.setAttribute("name", "currentImg");
-  console.log(prevImg);
-  console.log(prevImg.getAttribute("visible"));
 }
 
 function switchToNextPainting(e) {
   const locationName = e.target.getAttribute("name");
-  console.log(locationName);
   var nft = document.getElementById("nft_" + locationName);
-  console.log(nft);
   const imgCnt = nft.getAttribute("imgCnt");
-  console.log(imgCnt);
 
   // hide current image
   var currentImg = nft.querySelector("a-image[name='currentImg']");
-  console.log(currentImg);
-  console.log(currentImg.getAttribute("visible"));
   currentImg.setAttribute("visible", "false");
   currentImg.setAttribute("name", "");
-  console.log(currentImg);
-  console.log(currentImg.getAttribute("visible"));
 
   // display prev image
   const currentImgIndex = parseInt(currentImg.getAttribute("index"));
   const nextImgIndex = currentImgIndex === imgCnt - 1 ? 0 : currentImgIndex + 1;
   var nextImg = nft.querySelector("a-image[index='" + nextImgIndex + "']");
-  console.log(nextImg);
-  console.log(nextImg.getAttribute("visible"));
   nextImg.setAttribute("visible", "true");
   nextImg.setAttribute("name", "currentImg");
-  console.log(nextImg);
-  console.log(nextImg.getAttribute("visible"));
 }
