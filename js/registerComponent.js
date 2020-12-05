969e5b1b0872d3ad25f79ee21e52a7c476713eac
Hu Yong Hao <mlapenang1998@gmail.com>
2020-12-05 17:59:58 +0900

    adjust change img button

--- js/registerComponent.js
+++ js/registerComponent.js
@@ -11,13 +11,16 @@ AFRAME.registerComponent("markerhandler", {
           document.getElementById("newPaintCanvas").style.display = "block";
           document.getElementById("closeCanvasButton").style.display = "block";
           document.getElementById("colorPanel").style.display = "block";
+          document.getElementById("prevImgButton").style.display = "block";
+          document.getElementById("nextImgButton").style.display = "block";
           document.getElementById("save").setAttribute("name", locationName);
         });
       }
     });
     this.el.sceneEl.addEventListener("markerLost", e => {
-      var openCanvasButton = document.getElementById("openCanvasButton");
-      openCanvasButton.style.display = "none";
+      document.getElementById("openCanvasButton").style.display = "none";
+      document.getElementById("prevImgButton").style.display = "none";
+      document.getElementById("nextImgButton").style.display = "none";
     });
   }
 });
