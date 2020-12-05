f228d74a867d0f2ce4990976cceea56206f339f3
Hu Yong Hao <mlapenang1998@gmail.com>
2020-12-05 22:51:58 +0900

    test

--- js/paintingSwitch.js
+++ js/paintingSwitch.js
@@ -5,29 +5,31 @@ function switchToPrevPainting(e) {
 
   // hide current image
   var currentImg = nft.querySelector("a-image[visible='true']");
+  console.log(currentImg);
   currentImg.setAttribute("visible", false);
 
   // display prev image
   const currentImgIndex = parseInt(currentImg.getAttribute("index"));
   const prevImgIndex = currentImgIndex === 0 ? imgCnt - 1 : currentImgIndex - 1;
   var prevImg = nft.querySelector("a-image[index='" + prevImgIndex + "']");
+  console.log(prevImg);
   prevImg.setAttribute("visible", true);
 }
 
 function switchToNextPainting(e) {
   const locationName = e.target.getAttribute("location");
+  var nft = document.getElementById("nft_" + locationName);
+  const imgCnt = nft.getAttribute("imgCnt");
 
   // hide current image
-  var currentImg = document
-    .getElementById("nft_" + locationName)
-    .querySelector("a-image[visible='true']");
+  var currentImg = nft.querySelector("a-image[visible='true']");
+  console.log(currentImg);
   currentImg.setAttribute("visible", false);
 
   // display prev image
   const currentImgIndex = parseInt(currentImg.getAttribute("index"));
   const nextImgIndex = currentImgIndex === imgCnt - 1 ? 0 : currentImgIndex + 1;
-  var nextImg = document
-    .getElementById("nft_" + locationName)
-    .querySelector("a-image[index='" + nextImgIndex + "']");
+  var nextImg = nft.querySelector("a-image[index='" + nextImgIndex + "']");
+  console.log(nextImg);
   nextImg.setAttribute("visible", true);
 }
