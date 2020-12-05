5ab6eaeaec9c3b9a6b7d05fc00d5d38ed0124669
Hu Yong Hao <mlapenang1998@gmail.com>
2020-12-05 23:41:20 +0900

    test

--- js/paintingSwitch.js
+++ js/paintingSwitch.js
@@ -7,22 +7,38 @@ function switchToPrevPainting(e) {
   console.log(imgCnt);
 
   // hide current image
-  var currentImg = nft.querySelector("a-image[visible='true']");
-  console.log(currentImg);
-  console.log(currentImg.getAttribute("visible"));
-  currentImg.setAttribute("visible", "false");
-  console.log(currentImg);
-  console.log(currentImg.getAttribute("visible"));
+  console.log(nft.querySelector("a-image[visible='true']"));
+  console.log(
+    nft.querySelector("a-image[visible='true']").getAttribute("visible")
+  );
+  const currentImgIndex = parseInt(
+    nft.querySelector("a-image[visible='true']").getAttribute("index")
+  );
+  nft.querySelector("a-image[visible='true']").setAttribute("visible", "false");
+  console.log(nft.querySelector("a-image[index='" + currentImgIndex + "']"));
+  console.log(
+    nft
+      .querySelector("a-image[index='" + currentImgIndex + "']")
+      .getAttribute("visible")
+  );
 
   // display prev image
-  const currentImgIndex = parseInt(currentImg.getAttribute("index"));
   const prevImgIndex = currentImgIndex === 0 ? imgCnt - 1 : currentImgIndex - 1;
-  var prevImg = nft.querySelector("a-image[index='" + prevImgIndex + "']");
-  console.log(prevImg);
-  console.log(prevImg.getAttribute("visible"));
-  prevImg.setAttribute("visible", "true");
-  console.log(prevImg);
-  console.log(prevImg.getAttribute("visible"));
+  console.log(nft.querySelector("a-image[index='" + prevImgIndex + "']"));
+  console.log(
+    nft
+      .querySelector("a-image[index='" + prevImgIndex + "']")
+      .getAttribute("visible")
+  );
+  nft
+    .querySelector("a-image[index='" + prevImgIndex + "']")
+    .setAttribute("visible", "true");
+  console.log(nft.querySelector("a-image[index='" + prevImgIndex + "']"));
+  console.log(
+    nft
+      .querySelector("a-image[index='" + prevImgIndex + "']")
+      .getAttribute("visible")
+  );
 }
 
 function switchToNextPainting(e) {
