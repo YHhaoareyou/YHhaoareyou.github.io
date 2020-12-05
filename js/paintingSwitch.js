f54a1748065afd7c323c488ab435389733d4f9af
Hu Yong Hao <mlapenang1998@gmail.com>
2020-12-05 18:32:59 +0900

    split painting switches function to another js file

---
+++ js/paintingSwitch.js
@@ -0,0 +1,41 @@
+function setPrevImgSwitchListener() {
+  var prevImgButton = document.getElementById("prevImgButton");
+  prevImgButton.addEventListener("click", function() {
+    const locationName = e.target.getAttribute("name");
+    var nft = document.getElementById("nft_" + locationName);
+    const imgCnt = nft.getAttribute("imgCnt");
+
+    // hide current image
+    var currentImg = nft.querySelector("a-image[visible='true']");
+    currentImg.setAttribute("visible", "false");
+
+    // display prev image
+    const currentImgIndex = parseInt(currentImg.getAttribute("index"));
+    const prevImgIndex =
+      currentImgIndex === 0 ? imgCnt - 1 : currentImgIndex - 1;
+    var prevImg = nft.querySelector("a-image[index='" + prevImgIndex + "']");
+    prevImg.setAttribute("visible", "true");
+  });
+}
+
+function setNextImgSwitchListener() {
+  var nextImgButton = document.getElementById("nextImgButton");
+  nextImgButton.addEventListener("click", function() {
+    const locationName = e.target.getAttribute("name");
+
+    // hide current image
+    var currentImg = document
+      .getElementById("nft_" + locationName)
+      .querySelector("a-image[visible='true']");
+    currentImg.setAttribute("visible", "false");
+
+    // display prev image
+    const currentImgIndex = parseInt(currentImg.getAttribute("index"));
+    const nextImgIndex =
+      currentImgIndex === imgCnt - 1 ? 0 : currentImgIndex + 1;
+    var nextImg = document
+      .getElementById("nft_" + locationName)
+      .querySelector("a-image[index='" + nextImgIndex + "']");
+    nextImg.setAttribute("visible", "true");
+  });
+}
