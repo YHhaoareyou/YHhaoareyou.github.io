f32724eac42a0b63dc5ca0318429b86728c8bbf3
Hu Yong Hao <mlapenang1998@gmail.com>
2020-12-06 00:05:25 +0900

    remove unused attribute

--- js/registerComponent.js
+++ js/registerComponent.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent("markerhandler", {
   init: function() {
     this.el.sceneEl.addEventListener("markerFound", e => {
-      const locationName = e.target.getAttribute("location");
+      const locationName = e.target.id.replace("nft_", "");
 
       if (document.getElementById("newPaintCanvas").style.display !== "block") {
         // Display painting switching buttons
