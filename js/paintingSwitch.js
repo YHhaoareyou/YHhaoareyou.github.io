2a71cd43879ba640657d37c2ead491d13f6fae5b
Hu Yong Hao <mlapenang1998@gmail.com>
2020-12-06 00:09:32 +0900

    typo

--- js/paintingSwitch.js
+++ js/paintingSwitch.js
@@ -5,14 +5,14 @@ function switchToPrevPainting(e) {
 
   // hide current image
   var currentImg = nft.querySelector("a-image[name='currentImg']");
-  currentImg.setAttribute("visible", "false");
+  currentImg.setAttribute("visible", false);
   currentImg.setAttribute("name", "");
 
   // display prev image
   const currentImgIndex = parseInt(currentImg.getAttribute("index"));
   const prevImgIndex = currentImgIndex === 0 ? imgCnt - 1 : currentImgIndex - 1;
   var prevImg = nft.querySelector("a-image[index='" + prevImgIndex + "']");
-  prevImg.setAttribute("visible", "true");
+  prevImg.setAttribute("visible", true);
   prevImg.setAttribute("name", "currentImg");
 }
 
@@ -23,13 +23,13 @@ function switchToNextPainting(e) {
 
   // hide current image
   var currentImg = nft.querySelector("a-image[name='currentImg']");
-  currentImg.setAttribute("visible", "false");
+  currentImg.setAttribute("visible", false);
   currentImg.setAttribute("name", "");
 
   // display prev image
   const currentImgIndex = parseInt(currentImg.getAttribute("index"));
   const nextImgIndex = currentImgIndex === imgCnt - 1 ? 0 : currentImgIndex + 1;
   var nextImg = nft.querySelector("a-image[index='" + nextImgIndex + "']");
-  nextImg.setAttribute("visible", "true");
+  nextImg.setAttribute("visible", true);
   nextImg.setAttribute("name", "currentImg");
 }
