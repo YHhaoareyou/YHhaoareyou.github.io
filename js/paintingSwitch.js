35bea706ffabf6f0917314a22f2c7d7af6d26437
Hu Yong Hao <mlapenang1998@gmail.com>
2020-12-06 20:43:09 +0900

    adjust title & description style

--- js/paintingSwitch.js
+++ js/paintingSwitch.js
@@ -7,17 +7,24 @@ function switchToPrevPainting(e) {
   var currentImg = nft.querySelector("a-image[name='currentImg']");
   currentImg.setAttribute("visible", false);
   currentImg.setAttribute("name", "");
-  const currentImgIndex = parseInt(currentImg.getAttribute("index"));
-  nft.querySelector("div#imgTitle_" + currentImgIndex).style.display = "none";
-  nft.querySelector("div#imgDes_" + currentImgIndex).style.display = "none";
+
+  // hide current image info
+  const currentImgKey = currentImg.getAttribute("src").replace("#", "");
+  document.getElementById("div#imgTitle_" + currentImgKey).style.display =
+    "none";
+  document.getElementById("div#imgDes_" + currentImgKey).style.display = "none";
 
   // display prev image
+  const currentImgIndex = parseInt(currentImg.getAttribute("index"));
   const prevImgIndex = currentImgIndex === 0 ? imgCnt - 1 : currentImgIndex - 1;
   var prevImg = nft.querySelector("a-image[index='" + prevImgIndex + "']");
   prevImg.setAttribute("visible", true);
   prevImg.setAttribute("name", "currentImg");
-  nft.querySelector("div#imgTitle_" + prevImgIndex).style.display = "block";
-  nft.querySelector("div#imgDes_" + prevImgIndex).style.display = "block";
+
+  // display prev image info
+  const prevImgKey = prevImg.getAttribute("src").replace("#", "");
+  document.getElementById("div#imgTitle_" + prevImgKey).style.display = "none";
+  document.getElementById("div#imgDes_" + prevImgKey).style.display = "none";
 }
 
 function switchToNextPainting(e) {
@@ -29,15 +36,22 @@ function switchToNextPainting(e) {
   var currentImg = nft.querySelector("a-image[name='currentImg']");
   currentImg.setAttribute("visible", false);
   currentImg.setAttribute("name", "");
-  const currentImgIndex = parseInt(currentImg.getAttribute("index"));
-  nft.querySelector("div#imgTitle_" + currentImgIndex).style.display = "none";
-  nft.querySelector("div#imgDes_" + currentImgIndex).style.display = "none";
 
-  // display prev image
+  // hide current image info
+  const currentImgKey = currentImg.getAttribute("src").replace("#", "");
+  document.getElementById("div#imgTitle_" + currentImgKey).style.display =
+    "none";
+  document.getElementById("div#imgDes_" + currentImgKey).style.display = "none";
+
+  // display next image
+  const currentImgIndex = parseInt(currentImg.getAttribute("index"));
   const nextImgIndex = currentImgIndex === imgCnt - 1 ? 0 : currentImgIndex + 1;
   var nextImg = nft.querySelector("a-image[index='" + nextImgIndex + "']");
   nextImg.setAttribute("visible", true);
   nextImg.setAttribute("name", "currentImg");
-  nft.querySelector("div#imgTitle_" + nextImgIndex).style.display = "block";
-  nft.querySelector("div#imgDes_" + nextImgIndex).style.display = "block";
+
+  // display next image info
+  const nextImgKey = nextImg.getAttribute("src").replace("#", "");
+  document.getElementById("div#imgTitle_" + nextImgKey).style.display = "block";
+  document.getElementById("div#imgDes_" + nextImgKey).style.display = "block";
 }
