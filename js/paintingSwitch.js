e536fc45f85e8eb86317d72fb51657461ef369d1
Hu Yong Hao <mlapenang1998@gmail.com>
2020-12-06 20:35:23 +0900

    typo

--- js/paintingSwitch.js
+++ js/paintingSwitch.js
@@ -8,16 +8,16 @@ function switchToPrevPainting(e) {
   currentImg.setAttribute("visible", false);
   currentImg.setAttribute("name", "");
   const currentImgIndex = parseInt(currentImg.getAttribute("index"));
-  nft.querySelector("div#imgTitle_" + currentImgIndex).style.display = none;
-  nft.querySelector("div#imgDes_" + currentImgIndex).style.display = none;
+  nft.querySelector("div#imgTitle_" + currentImgIndex).style.display = "none";
+  nft.querySelector("div#imgDes_" + currentImgIndex).style.display = "none";
 
   // display prev image
   const prevImgIndex = currentImgIndex === 0 ? imgCnt - 1 : currentImgIndex - 1;
   var prevImg = nft.querySelector("a-image[index='" + prevImgIndex + "']");
   prevImg.setAttribute("visible", true);
   prevImg.setAttribute("name", "currentImg");
-  nft.querySelector("div#imgTitle_" + prevImgIndex).style.display = block;
-  nft.querySelector("div#imgDes_" + prevImgIndex).style.display = block;
+  nft.querySelector("div#imgTitle_" + prevImgIndex).style.display = "block";
+  nft.querySelector("div#imgDes_" + prevImgIndex).style.display = "block";
 }
 
 function switchToNextPainting(e) {
@@ -30,14 +30,14 @@ function switchToNextPainting(e) {
   currentImg.setAttribute("visible", false);
   currentImg.setAttribute("name", "");
   const currentImgIndex = parseInt(currentImg.getAttribute("index"));
-  nft.querySelector("div#imgTitle_" + currentImgIndex).style.display = none;
-  nft.querySelector("div#imgDes_" + currentImgIndex).style.display = none;
+  nft.querySelector("div#imgTitle_" + currentImgIndex).style.display = "none";
+  nft.querySelector("div#imgDes_" + currentImgIndex).style.display = "none";
 
   // display prev image
   const nextImgIndex = currentImgIndex === imgCnt - 1 ? 0 : currentImgIndex + 1;
   var nextImg = nft.querySelector("a-image[index='" + nextImgIndex + "']");
   nextImg.setAttribute("visible", true);
   nextImg.setAttribute("name", "currentImg");
-  nft.querySelector("div#imgTitle_" + prevImgIndex).style.display = block;
-  nft.querySelector("div#imgDes_" + prevImgIndex).style.display = block;
+  nft.querySelector("div#imgTitle_" + nextImgIndex).style.display = "block";
+  nft.querySelector("div#imgDes_" + nextImgIndex).style.display = "block";
 }
