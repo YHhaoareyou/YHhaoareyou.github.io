64faa8fe4160b733beaaedcadc9f53121764ba71
Hu Yong Hao <mlapenang1998@gmail.com>
2020-12-06 15:19:49 +0900

    adjust toolPanel position

--- js/canvasConfig.js
+++ js/canvasConfig.js
@@ -1,7 +1,9 @@
 function closeCanvas() {
   document.getElementById("newPaintCanvas").style.display = "none";
   document.getElementById("closeCanvasButton").style.display = "none";
-  document.getElementById("toolPanel").style.display = "none";
+  document.getElementById("colorPanel").style.display = "none";
+  document.getElementById("brushPanel").style.display = "none";
+  document.getElementById("actionPanel").style.display = "none";
 }
 
 function saveCanvas(canvas, locationName) {
