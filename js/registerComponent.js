25bfc8f0f95faf27400c14dc58a0382c1e93a072
YHhaoareyou <mlapenang1998@gmail.com>
2021-01-10 15:40:59 +0900

    show error with alert

--- js/registerComponent.js
+++ js/registerComponent.js
@@ -1,36 +1,42 @@
 AFRAME.registerComponent("markerhandler", {
-  init: function() {
-    this.el.sceneEl.addEventListener("markerFound", e => {
+  init: function () {
+    this.el.sceneEl.addEventListener("markerFound", (e) => {
       // const locationName = e.target.getAttribute("location");
-      const locationName = e.target.id.replace("nft_", ""); // Display painting switching buttons
-      var prevImgButton = document.getElementById("prevImgButton");
-      var nextImgButton = document.getElementById("nextImgButton");
-      prevImgButton.style.display = "block";
-      nextImgButton.style.display = "block";
-      prevImgButton.setAttribute("name", locationName);
-      nextImgButton.setAttribute("name", locationName);
+      try {
+        const locationName = e.target.id.replace("nft_", ""); // Display painting switching buttons
+        var prevImgButton = document.getElementById("prevImgButton");
+        var nextImgButton = document.getElementById("nextImgButton");
+        prevImgButton.style.display = "block";
+        nextImgButton.style.display = "block";
+        prevImgButton.setAttribute("name", locationName);
+        nextImgButton.setAttribute("name", locationName);
 
-      var openCanvasButton = document.getElementById("openCanvasButton");
-      openCanvasButton.style.display = "block";
-      openCanvasButton.addEventListener("click", e => {
-        document.getElementById("newPaintCanvas").style.display = "block";
-        document.getElementById("closeCanvasButton").style.display = "block";
-        document.getElementById("toolPanel").style.display = "block";
-        document.getElementById("save").setAttribute("name", locationName);
-      });
+        var openCanvasButton = document.getElementById("openCanvasButton");
+        openCanvasButton.style.display = "block";
+        openCanvasButton.addEventListener("click", (e) => {
+          document.getElementById("newPaintCanvas").style.display = "block";
+          document.getElementById("closeCanvasButton").style.display = "block";
+          document.getElementById("toolPanel").style.display = "block";
+          document.getElementById("save").setAttribute("name", locationName);
+        });
 
-      if (document.querySelector("#nft_" + locationName + " a-image")) {
-        const key = document
-          .querySelector("#nft_" + locationName + " a-image[name='currentImg']")
-          .getAttribute("src")
-          .replace("#", "");
-        // display first painting info
-        document.getElementById("imgInfo_" + key).style.display = "block";
+        if (document.querySelector("#nft_" + locationName + " a-image")) {
+          const key = document
+            .querySelector(
+              "#nft_" + locationName + " a-image[name='currentImg']"
+            )
+            .getAttribute("src")
+            .replace("#", "");
+          // display first painting info
+          document.getElementById("imgInfo_" + key).style.display = "block";
 
-        document.getElementById("likeButton").style.display = "block";
+          document.getElementById("likeButton").style.display = "block";
+        }
+      } catch (e) {
+        alert(e);
       }
     });
-    this.el.sceneEl.addEventListener("markerLost", e => {
+    this.el.sceneEl.addEventListener("markerLost", (e) => {
       document.getElementById("openCanvasButton").style.display = "none";
       document.getElementById("prevImgButton").style.display = "none";
       document.getElementById("nextImgButton").style.display = "none";
@@ -46,5 +52,5 @@ AFRAME.registerComponent("markerhandler", {
       document.getElementById("imgInfo_" + key).style.display = "none";
       document.getElementById("likeButton").style.display = "none";
     });
-  }
+  },
 });
