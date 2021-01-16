566a66e915308975cebd8daeddec94ece9afac07
YHhaoareyou <mlapenang1998@gmail.com>
2021-01-16 17:30:49 +0900

    debug

--- js/likeConfig.js
+++ js/likeConfig.js
@@ -4,8 +4,9 @@ function cancelLike(imgId, locationName, uid) {}
 
 function configLike(uid) {
   var likeButton = document.getElementById("likeButton");
-
-  likeButton.addEventListener("click", (e) => {
-    console.log("like button clicked");
-  });
+  if (likeButton) {
+    likeButton.addEventListener("click", (e) => {
+      console.log("like button clicked");
+    });
+  }
 }
