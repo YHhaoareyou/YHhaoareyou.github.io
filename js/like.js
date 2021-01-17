8d05a77742c25058bb927dd77a9a2eac5a291c69
YHhaoareyou <mlapenang1998@gmail.com>
2021-01-18 00:54:00 +0900

    Display like count on likeButton

--- js/like.js
+++ js/like.js
@@ -2,38 +2,36 @@ function like(imgId, locationName, uid) {
   const likesNode = database.ref().child(locationName + "/" + imgId + "/likes");
 
   likesNode.once("value").then((snap) => {
-    likesNode
-      .set({
-        counts: snap.val() ? snap.val().counts + 1 : 0,
-        uids: snap.val() ? [snap.val().uids, uid].join(",") : uid,
-      })
-      .then((snap) => {
-        alert("Liked!");
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  });
-}
-
-function cancelLike(imgId, locationName, uid) {
-  const likesNode = database.ref().child(locationName + "/" + imgId + "/likes");
-
-  likesNode.once("value").then((snap) => {
-    likesNode
-      .set({
-        counts: snap.val().counts - 1,
-        uids: snap
-          .val()
-          .uids.split(",")
-          .filter((id) => id !== uid)
-          .join(","),
-      })
-      .then((snap) => {
-        alert("Like canceled.");
-      })
-      .catch((error) => {
-        alert(error);
-      });
+    const val = snap.val();
+    if (val && val.uids.includes(uid)) {
+      likesNode
+        .set({
+          counts: val.counts - 1,
+          uids: val.uids
+            .split(",")
+            .filter((id) => id !== uid)
+            .join(","),
+        })
+        .then((snap) => {
+          alert("Like canceled.");
+          $("#likeButton").text("♥ " + snap.val().counts);
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    } else {
+      likesNode
+        .set({
+          counts: val ? val.counts + 1 : 1,
+          uids: val ? [val.uids, uid].join(",") : uid,
+        })
+        .then((snap) => {
+          alert("Liked!");
+          $("#likeButton").text("♥ " + snap.val().counts);
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    }
   });
 }
--- js/registerComponent.js
+++ js/registerComponent.js
@@ -12,38 +12,47 @@ AFRAME.registerComponent("markerhandler", {
         prevImgButton.attr("name", locationName);
         nextImgButton.attr("name", locationName);
 
-        var openCanvasButton = $("#openCanvasButton");
-        if (openCanvasButton) {
-          openCanvasButton.css("display", "block");
-          openCanvasButton.on("click", (e) => {
-            $("#newPaintCanvas").css("display", "block");
-            $("#closeCanvasButton").css("display", "block");
-            $("#toolPanel").css("display", "block");
-            $("#save").attr("name", locationName);
-          });
-        }
-
-        var aImage = $("#nft_" + locationName + " a-image[name='currentImg']");
-        if (aImage) {
-          const key = aImage.attr("src").replace("#", "");
-          // display first painting info
-          $("#imgInfo_" + key).css("display", "block");
-
-          var likeButton = $("#likeButton");
-          if (likeButton) {
-            likeButton.css("display", "block");
-            likeButton.attr("name", locationName);
-            likeButton.on("click", (e) => {
-              var uid = $("#state").data("uid");
-              if (uid && uid !== "") {
-                const imgId = $(
-                  "#nft_" + locationName + " a-image[name='currentImg']"
-                )
-                  .attr("src")
-                  .substring(1);
-                like(imgId, locationName, uid);
-              }
+        // If login: display openCanvasButton & likeButton
+        const uid = $("#state").data("uid");
+        if (uid) {
+          // display openCanvasButton
+          var openCanvasButton = $("#openCanvasButton");
+          if (openCanvasButton) {
+            openCanvasButton.css("display", "block");
+            // openCanvasButton onclick: display canvas
+            openCanvasButton.on("click", (e) => {
+              $("#newPaintCanvas").css("display", "block");
+              $("#closeCanvasButton").css("display", "block");
+              $("#toolPanel").css("display", "block");
+              $("#save").attr("name", locationName);
             });
+          }
+
+          var aImage = $(
+            "#nft_" + locationName + " a-image[name='currentImg']"
+          );
+          if (aImage) {
+            const imgId = aImage.attr("src").replace("#", "");
+            // display first painting info
+            $("#imgInfo_" + imgId).css("display", "block");
+
+            // display likeButton
+            var likeButton = $("#likeButton");
+            if (likeButton) {
+              const likesNode = database
+                .ref()
+                .child(locationName + "/" + imgId + "/likes");
+              likesNode.once("value").then((snap) => {
+                if (snap.val()) likeButton.text("♥ " + snap.val().counts);
+              });
+              likeButton.css("display", "block");
+              likeButton.attr("name", locationName);
+              // likeButton onclick: like!
+              likeButton.on("click", (e) => {
+                var uid = $("#state").data("uid");
+                like(imgId, locationName, uid);
+              });
+            }
           }
         }
       } catch (e) {
@@ -52,10 +61,10 @@ AFRAME.registerComponent("markerhandler", {
     });
 
     this.el.sceneEl.addEventListener("markerLost", (e) => {
-      var openCanvasButton = document.getElementById("openCanvasButton");
-      if (openCanvasButton) openCanvasButton.style.display = "none";
-      document.getElementById("prevImgButton").style.display = "none";
-      document.getElementById("nextImgButton").style.display = "none";
+      $("#openCanvasButton").css("display", "none");
+      $("#prevImgButton").css("display", "none");
+      $("#nextImgButton").css("display", "none");
+      $("likeButton").css("display", "none");
       // hide painting info
       const key = $(
         "#nft_" +
@@ -64,9 +73,7 @@ AFRAME.registerComponent("markerhandler", {
       )
         .attr("src")
         .replace("#", "");
-      document.getElementById("imgInfo_" + key).style.display = "none";
-      var likeButton = document.getElementById("likeButton");
-      if (likeButton) likeButton.style.display = "none";
+      $("#imgInfo_" + key).css("display", "none");
     });
   },
 });
